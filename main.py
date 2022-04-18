6a1c75b0ff77bbfd453172e6b8e7e5897b51de35
Timo Ramsbrock <tramsbrock@gmail.com>
2022-04-18 11:18:32 +0200

    New Vosk system

--- main.py
+++ main.py
@@ -1,116 +1,53 @@
-#!/usr/bin/env python3
-
-import argparse
-from cgitb import text
-import os
-import queue
-import sounddevice as sd
-import vosk
-import sys
+"""Test Vosk microphone."""
 import json
-import re
+import os
+import sys
+
+import pyaudio
 import pyttsx3
-from modules.text2int import *
+
+from vosk import KaldiRecognizer
+from vosk import Model
 import command_handler.command_handler as command
 
 prefix='q '
 
-q = queue.Queue()
-
 engine = pyttsx3.init()
 
-def contains_word(s, w):
-    return (' ' + w + ' ') in (' ' + s + ' ')
+if not os.path.exists("model"):
+    print("Please download the model and unpack as 'model' in the current folder.")
+    sys.exit()
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
+MODEL = Model("model")
+REC = KaldiRecognizer(MODEL, 16000)
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    '-f', '--filename', type=str, metavar='FILENAME',
-    help='audio file to store recording to')
-parser.add_argument(
-    '-m', '--model', type=str, metavar='MODEL_PATH',
-    help='Path to the model')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-r', '--samplerate', type=int, help='sampling rate')
-args = parser.parse_args(remaining)
+P = pyaudio.PyAudio()
+STREAM = P.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=16000,
+    input=True,
+    frames_per_buffer=8000)
+STREAM.start_stream()
 
-try:
-    if args.model is None:
-        args.model = "model"
-    if not os.path.exists(args.model):
-        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-        print ("and unpack as 'model' in the current folder.")
-        parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        args.samplerate = int(device_info['default_samplerate'])
-
-    model = vosk.Model(args.model)
-
-    if args.filename:
-        dump_fn = open(args.filename, "wb")
-    else:
-        dump_fn = None
-
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
-
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    strValue=rec.Result()
-                    eval = json.loads(strValue)
-                    wval = eval['text']
-                    print(wval)
-                    if wval == "":
-                        wval = ""
-                    elif (" ") + prefix in (" " + wval + " "):
-                        before, sep, after = wval.partition(prefix)
-                        if len(after) > 0:
-                            wval = after
-                        x = wval.replace(prefix, "")
-                        print(x)
-                        oput = command.h(x)
-                        engine.say(oput)
-                        engine.runAndWait()
-                #else:
-                #    print(rec.PartialResult())
-                if dump_fn is not None:
-                    dump_fn.write(data)
-
-except KeyboardInterrupt:
-    print('\nDone')
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+while True:
+    DATA = STREAM.read(14000)
+    if len(DATA) == 0:
+        pass
+    if REC.AcceptWaveform(DATA):
+        KIRA_RESULT = REC.Result()
+        KIRA_PARSED_JSON = json.loads(KIRA_RESULT)
+        wval = KIRA_PARSED_JSON['text']
+        print(wval)
+        if wval == "":
+            wval = ""
+        elif (" ") + prefix in (" " + wval + " "):
+            before, sep, after = wval.partition(prefix)
+            if len(after) > 0:
+                wval = after
+            x = wval.replace(prefix, "")
+            print(x)
+            oput = command.h(x)
+            engine.say(oput)
+            engine.runAndWait()
