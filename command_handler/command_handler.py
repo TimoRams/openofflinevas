a39b965f8f4b2fae9a975c22543918a0a112943e
TimoRams <timorams@hotmail.com>
2022-06-06 09:37:43 +0200

    added stop function

--- command_handler/command_handler.py
+++ command_handler/command_handler.py
@@ -1,7 +1,9 @@
 import commands.sysinfo as si
-
+import sys
 def h(input):
-    if input == 'info':
+    if input == 'stop':
+        sys.exit(0)
+    elif input == 'info':
         si.System_information()
         returnvar = si.System_information()
     else:
