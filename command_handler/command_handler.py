5ada4d6d40c6920c3307ae10b71eed228a824d69
TimoRams <timorams@hotmail.com>
2022-06-06 10:39:18 +0200

    added utility command

--- command_handler/command_handler.py
+++ command_handler/command_handler.py
@@ -1,11 +1,18 @@
 import commands.sysinfo as si
+import commands.utility as util
+
 import sys
+
+
 def h(input):
     if input == 'stop':
         sys.exit(0)
     elif input == 'info':
         si.System_information()
         returnvar = si.System_information()
+    elif input == 'time':
+        util.time()
+        returnvar = util.time()
     else:
         returnvar = 'Command not found'
     return returnvar
