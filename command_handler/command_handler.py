507b097131d0bc60c8f61fa04553a177d7640fa7
TimoRams <timorams@hotmail.com>
2022-06-07 20:09:01 +0200

    added torch

--- command_handler/command_handler.py
+++ command_handler/command_handler.py
@@ -1,7 +1,35 @@
+import random
+import json
+
+import torch
+
+from modules.model import NeuralNet
+from modules.nltk_utils import bag_of_words, tokenize
+
 import commands.sysinfo as si
 import commands.utility as util
 
 import sys
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
 
 
 def h(input):
@@ -14,5 +42,23 @@ def h(input):
         util.time()
         returnvar = util.time()
     else:
-        returnvar = 'Command not found'
-    return returnvar
+        sentence = input
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, all_words)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
+
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
+
+        tag = tags[predicted.item()]
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    return random.choice(intent['responses'])
+        else:
+            return "I do not understand..."
+
